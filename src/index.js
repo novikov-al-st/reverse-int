d4ecf822cb8c303f1ffa118003f2230b526917c1
Novikov Alexandr <novikov.al.st@gmail.com>
2022-01-08 04:48:26 +0200

    feat: implement method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let numberString = String(n);
+    let numberReverseString = '';
+
+    if (numberString[0] === '-') {
+        numberString = numberString.substring(0);
+    }
+
+    for (let i = numberString.length - 1; i >= 0; i--) {
+        numberReverseString += numberString[i];
+    }
+
+    return parseInt(numberReverseString);
 }
